54ef983899443f0ba0993128ab8aad3b8f705e33
richard wei <therichardwei@gmail.com>
2023-07-29 13:35:43 -0400

    Add java script to generate expr classes and printer for ast using visitor pattern

--- com/craftinginterpreters/lox/Lox.java
+++ com/craftinginterpreters/lox/Lox.java
@@ -26,6 +26,9 @@ public class Lox {
         byte[] bytes = Files.readAllBytes(Paths.get(path));
         run(new String(bytes, Charset.defaultCharset()));
 
+        // for testing only
+        printAst();
+
         if (hadError) System.exit(65);
     }
 
@@ -63,6 +66,18 @@ public class Lox {
     private static void report(int line, String where, String message) {
         System.err.println("[line " + line + "] Error" + where + ": " + message);
         hadError = true;
+    }
+
+    private static void printAst() { 
+        Expr expression = new Expr.Binary(
+                new Expr.Unary(
+                    new Token(TokenType.MINUS, "-", null, 1),
+                    new Expr.Literal(123)),
+                new Token(TokenType.STAR, "*", null, 1),
+                new Expr.Grouping(
+                    new Expr.Literal(45.67)));
+
+        System.out.println(new AstPrinter().print(expression));
     }
 }
 
